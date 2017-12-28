bdc7f2a6d297eeb8616df1188987df59af72935d
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-28 12:55:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,8 @@ function prependKitten(name) {
   var newArray = [name, ...kittens];
   return newArray;
 }
-function removeLastKitten() {}
+function removeLastKitten() {
+  var newArray = kittens.slice(0, kittens.length-1);
+  return newArray;
+}
 function removeFirstKitten() {}
