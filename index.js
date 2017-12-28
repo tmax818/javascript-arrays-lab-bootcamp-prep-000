ff3940cbd943eee2ebce0e13eb1b60174a836395
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-28 12:47:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = "I don't do much.";
 
 function destructivelyAppendKitten(name) {
-  
+  kittens.push(name);
+  return kittens;
 }
 function destructivelyPrependKitten(name) {}
 function destructivelyRemoveLastKitten() {}
